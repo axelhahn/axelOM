458439f66845514866bba4f1cff74fd98b45d4f4
Axel Hahn <ax2002@gmx.net>
2024-06-17 00:08:58 +0200

    update 404 page

--- public_html/admin/pages/error404.php
+++ public_html/admin/pages/error404.php
@@ -11,14 +11,20 @@
  * 
  */
 
-$TITLE='404 :: Page not found';
+$TITLE='{{404.title}}';
 $BODY=''
-    . '<i class="fa-solid fa-bug" style="font-size: 800%;"></i><br><br>'
+    . '<i class="'.icon::getclass('404').'" style="font-size: 800%;"></i><br><br>'
     . $renderAdminLTE->getAlert(array (
             'type' => 'danger',
-            'title' => 'The file was not found.',
+            'title' => '{{404.subtitle}}',
             'dismissible' => 0,
-            'text' => 'Please check the url or start again from <a href="?">HOME</a>.',
+            'text' => '{{404.message}}',
         )
     )
+    .'<br><br>'
+    . $renderAdminLTE->getButton([
+        'type' => 'secondary',
+        'text' => icon::get('back') . '{{back}}',
+        'onclick' => 'history.back();',
+        ])
     ;
