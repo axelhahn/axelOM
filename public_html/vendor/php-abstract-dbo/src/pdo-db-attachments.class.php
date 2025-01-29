c8ad743b13d7f280bd80e934e7fbe793bdb0075f
Axel Hahn <ax2002@gmx.net>
2025-01-30 00:16:04 +0100

    update abstract PDO classes

--- public_html/vendor/php-abstract-dbo/src/pdo-db-attachments.class.php
+++ public_html/vendor/php-abstract-dbo/src/pdo-db-attachments.class.php
@@ -33,6 +33,74 @@ class pdo_db_attachments extends pdo_db_base{
     //
     // ----------------------------------------------------------------------
 
+    /**
+     * Get array of all hook actions
+     * Keys:
+     *   - backend_preview   {string}  method name for preview in AxelOM
+     *   - <any>             {string}  method name four your custom action
+     * 
+     * @return array
+     */
+    public function hookActions(): array
+    {
+        return [
+            'backend_preview' => 'attachmentPreview',
+            'backend_upload' => 'attachmentPreview',
+        ];
+    }
+
+    /**
+     * Get html code for attachment preview
+     * 
+     * @param array  $aOptions  array of options; known keys:
+     *                          - baseurl {string}  set base url for attachments that triggers -> setUrlBase(<baseurl>)
+     * 
+     * @return string
+     */
+    public function attachmentPreview(array $aOptions = []): string
+    {
+        $sReturn = '';
+        $aFile = $this->getItem();
+        if (!$aFile) {
+            $this->_log(PB_LOGLEVEL_ERROR, __METHOD__, 'Item not found. Use read(<ID>) first.');
+            return $sReturn;
+        }
+        if($aOptions['baseurl']??false){
+            $this->setUrlBase($aOptions['baseurl']);
+        }
+
+        $sAttachmentUrl=$this->_sUrlBase . '/' . $aFile['filename'];
+
+        switch($aFile['mime']){
+            // ---- audio
+            case 'audio/mpeg':
+            case 'audio/ogg':
+            case 'audio/wav':
+                $sReturn .= "<audio controls><source src=\"$sAttachmentUrl\" type=\"$aFile[mime]\"></audio>";
+                break;
+
+            // ---- images
+            case 'image/gif':
+            case 'image/jpeg':
+            case 'image/png':
+                $sReturn .= "<img src=\"$sAttachmentUrl\" alt=\"$aFile[filename]\">";
+                break;
+
+            // ---- video
+            case 'video/mp4':
+            case 'video/ogg':
+                $sReturn .= "<video controls><source src=\"$sAttachmentUrl\" type=\"$aFile[mime]\"></video>";
+                break;
+        }
+
+        $sReturn.=''
+            . ($sReturn ? '<br><br>' : '')
+            . '<a href="' . $sAttachmentUrl . '" target="_blank">' . basename($aFile['filename']) . '</a><br>'
+            .$aFile['mime'].'<br>'
+            ;
+
+        return $sReturn ? "<div class=\"preview\">$sReturn</div>" : '';
+    }
 
     // ----------------------------------------------------------------------
     // SETTER
@@ -54,7 +122,7 @@ class pdo_db_attachments extends pdo_db_base{
     }
 
     /**
-     * Sets the base URL for the file attachments.
+     * Sets the base URL for the file attachments
      *
      * @param string $sUrl The base URL to set.
      * @return bool
